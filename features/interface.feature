5e34a48abfa122ad35217ca70159ab8f81290b7b
Samu Voutilainen <smar@smar.fi>
2016-01-01 18:13:23 +0200

    Tests for interface

---
+++ features/interface.feature
@@ -0,0 +1,19 @@
+Feature: We have CLI interface, which can be used for various tasks, like migrations and bootstrapping an environment
+
+  @project_test
+  Scenario: I have existing project, which suddenly is in need of a database. I want to employ multi_ar for that project.
+    Given I have existing project named "interface_test_project"
+    And project "interface_test_project" has existing bundle with multi_ar
+    When I run "multi_ar --init '.' -d interface_test_db" for that project
+    Then there should be following files:
+      | README.md |
+      | config/database.yaml |
+    And "README.md" should mention "interface_test_project"
+    And "config/database.yaml" should contain database config for "interface_test_db"
+
+  @project_test
+  Scenario: After I have integrated multi_ar to my project, I want to use it to generate some migrations
+    Given I have a multi_ar project named "interface_test_project" with database "interface_test_project"
+    When I run "multi_ar -d interface_test_project -t 'db:new_migration[CreateTestTable, test_value:string]'" for that project
+    And I run "multi_ar -d interface_test_project -t db:migrate -e test" for that project
+    Then table "test_tables" in database "interface_test_project" should contain field "test_value"
---
+++ features/step_definitions/interface_steps.rb
@@ -0,0 +1,98 @@
+
+require "fileutils"
+
+After("@project_test") do
+  FileUtils.remove_entry_secure("/tmp/#{@project_name}")
+end
+
+Given(/^I have existing project named "([^"]*)"$/) do |project_name|
+  Dir.chdir "/tmp" do
+    @project_name = project_name
+    Dir.mkdir project_name
+    Dir.chdir project_name do
+      File.open "Gemfile", "w" do |f|
+        gemfile = <<-EOF
+        # A sample Gemfile
+        source 'https://rubygems.org'
+        EOF
+        f.write gemfile
+      end
+    end
+  end
+end
+
+Given(/^project "([^"]*)" has existing bundle with multi_ar$/) do |project_name|
+  Dir.chdir "/tmp" do
+    Dir.chdir project_name do
+      File.open "Gemfile", "w" do |f|
+        gemfile = <<-EOF
+        # A sample Gemfile
+        source "https://rubygems.org"
+
+        gem "multi_ar"
+        EOF
+        f.write gemfile
+      end
+
+      `bundle install`
+      expect($?.exitstatus).to eq(0)
+    end
+  end
+end
+
+
+When(/^I run "([^"]*)" for that project$/) do |command|
+  Dir.chdir "/tmp" do
+    Dir.chdir @project_name do
+      puts `#{command}`
+      expect($?.exitstatus).to eq(0)
+    end
+  end
+end
+
+Then(/^there should be following files:$/) do |table|
+  # table is a Cucumber::Core::Ast::DataTable
+
+  table.raw.each do |row|
+    expect(File.exist? row[0]).to be_truthy
+  end
+end
+
+Then(/^"([^"]*)" should mention "([^"]*)"$/) do |filename, content|
+  expect(File.open("/tmp/#{@project_name}/#{filename}").readlines.join).to include(content)
+end
+
+Then(/^"([^"]*)" should contain database config for "([^"]*)"$/) do |filename, database|
+  expect(File.open("/tmp/#{@project_name}/#{filename}").readlines.join).to include("#{database}_development:")
+end
+
+Given(/^I have a multi_ar project named "([^"]*)" with database "([^"]*)"$/) do |project_name, database|
+  @project_name = project_name
+  Dir.chdir "/tmp" do
+    command = "multi_ar --init #{project_name} -d #{database}"
+    #puts "Running #{command}"
+    puts `#{command}`
+    expect($?.exitstatus).to eq(0)
+  end
+end
+
+Then(/^table "([^"]*)" in database "([^"]*)" should contain field "([^"]*)"$/) do |table, database, field|
+  Dir.chdir "/tmp" do
+    Dir.chdir @project_name do
+      db = SQLite3::Database.new "db/#{database}_test.sqlite3"
+      rows = db.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
+      puts rows.inspect
+      expect(rows.count).to eq(3)
+      expect(rows[0][0]).to eq("schema_migrations")
+      expect(rows[1][0]).to eq(table)
+      expect(rows[2][0]).to eq("sqlite_sequence")
+
+      rows = db.execute("pragma table_info('#{table}')")
+      puts rows.inspect
+      expect(rows.count).to eq(2)
+      expect(rows[0][1]).to eq("id")
+      expect(rows[1][1]).to eq(field)
+    end
+  end
+end
+
