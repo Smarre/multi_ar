ea1f9917f0d012844af2f88616cd4821e792af5a
Samu Voutilainen <smar@smar.fi>
2019-09-08 15:48:08 +0300

    Reformatted.

--- lib/multi_ar/rake/ext.rb
+++ lib/multi_ar/rake/ext.rb
@@ -2,10 +2,13 @@
 require "rake"
 
 # @api private
+#
 # Extension to Rake to provide rename task.
 module Rake
+
   # Extension to Rake to provide rename task.
   class Application
+
     # A Rake task to rename another Rake task.
     def rename_task(task, oldname, newname)
         if @tasks.nil?
@@ -19,23 +22,24 @@ module Rake
         end
     end
   end
-end
 
-# add new rename method to Rake::Task class
-# to rename a task
-class Rake::Task
-  # Renames current Rake task.
-  def rename(new_name)
-    if !new_name.nil?
-        old_name = @name
+  # Adds new method, rename, to Rake::Task class to rename a task.
+  class Task
+    # Renames current Rake task.
+    def rename(new_name)
+      if !new_name.nil?
+          old_name = @name
 
-        if old_name == new_name
-          return
-        end
+          if old_name == new_name
+            return
+          end
 
-        @name = new_name.to_s
-        application.rename_task(self, old_name, new_name)
+          @name = new_name.to_s
+          application.rename_task(self, old_name, new_name)
+      end
     end
   end
 end
+
+
 
